1ef9e0cba8e2fc472b7ee9250d45e8e0b032b365
Yassir Al-Refaie <yassir@152.3.123.172.rev.iijmobile.jp>
2023-10-09 17:01:38 +0100

    SD Card Writes working now

---
+++ PicoCart.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "sd_card.h"
+#include "ff.h"
+
+
+
+int main()
+{
+    FRESULT fr;
+    FATFS fs;
+    FIL fil;
+    int ret;
+    char buf[100];
+    char filename[] = "test03.txt";
+    stdio_init_all();
+     // Initialize SD card
+    if (!sd_init_driver()) {
+        printf("ERROR: Could not initialize SD card\r\n");
+        while (true);
+    }
+
+    // Mount drive
+    fr = f_mount(&fs, "0:", 1);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Open file for writing ()
+    fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not open file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Write something to file
+    ret = f_printf(&fil, "This is another test\r\n");
+    if (ret < 0) {
+        printf("ERROR: Could not write to file (%d)\r\n", ret);
+        f_close(&fil);
+        while (true);
+    }
+    ret = f_printf(&fil, "of writing to an SD card.\r\n");
+    if (ret < 0) {
+        printf("ERROR: Could not write to file (%d)\r\n", ret);
+        f_close(&fil);
+        while (true);
+    }
+
+    // Close file
+    fr = f_close(&fil);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not close file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Unmount drive
+    f_unmount("0:");
+
+    // Loop forever doing nothing
+    while (true) {
+        sleep_ms(1000);
+    }
+
+
+    puts("Hello, world!");
+
+    return 0;
+}
