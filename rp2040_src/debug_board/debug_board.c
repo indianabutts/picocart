0dc4c0685d6681423f61ed0e13f4b25b6cd5e2ac
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2024-03-24 12:12:03 +0000

    Initial Printing to LCD Display

--- rp2040_src/debug_board/debug_board.c
+++ rp2040_src/debug_board/debug_board.c
@@ -6,11 +6,11 @@
 #include "hardware/i2c.h"
 
 #include "includes/ssd1306.h"
-#include "includes/font.h"
+
 
 
 void setup_gpios(void);
-
+void print_message(void);
 int main(){
   stdio_init_all();
 
@@ -18,7 +18,7 @@ int main(){
 //  gpio_init_mask(D_GPIO_INIT_MASK);
 
   setup_gpios();
-  
+  print_message();
   return 0;
 }
 
@@ -32,19 +32,26 @@ void setup_gpios(void){
 
 
 void print_message(void) {
-  const char *message = "PicoCart Debug";
+    const char *message[] = {"PicoCart Debug", "I love", "Baba and Mama"};
 
   ssd1306_t disp;
   disp.external_vcc = false;
   ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
   ssd1306_clear(&disp);
+  ssd1306_draw_string(&disp, 0, 0, 1, message[0]);
+  for (int i=1; i<3; i++){
+      ssd1306_draw_string(&disp, 0, (i+1)*10, 1, message[i]);
+  
+  }
+  ssd1306_show(&disp);
+  sleep_ms(25);
+
   while(true){
-      ssd1306_draw_string(&disp, 0, 0, 1, message);
-      ssd1306_show(&disp);
-      sleep_ms(25);
-      ssd1306_clear(&disp);
       
   }
 }
+
+
+
 
 
